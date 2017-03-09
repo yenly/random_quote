822930e1b03a1e92e294686fc2ab503f15b34c39
Yenly Ma <yencodes@gmail.com>
2017-03-09 15:54:54 -0800

    Install firebase and add config settings for app.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import * as firebase from 'firebase';
+import { FirebaseConfig } from './keys';
+
+var config = {
+    apiKey: FirebaseConfig.apiKey,
+    authDomain: FirebaseConfig.authDomain,
+    databaseURL: FirebaseConfig.databaseURL,
+    storageBucket: FirebaseConfig.storageBucket,
+    messagingSenderId: FirebaseConfig.messagingSenderId
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <App />,
