45310cd2d303d87e0ebdf462570cfd9b087b5c44
Yenly Ma <yencodes@gmail.com>
2017-03-10 09:54:31 -0800

    Connect to firebase db and display data.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 import Quote from './components/quote';
+import * as firebase from 'firebase';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      quotes: {}
+    };
+  }
+
+  componentDidMount() {
+    const key = 1;
+    const rootRef = firebase.database().ref().child('quotes');
+    const quotesRef = rootRef.child(key);
+    quotesRef.on('value', snap => {
+      this.setState({
+        quotes: snap.val()
+      });
+    });
+  }
+
   render() {
+    console.log(this.state.quotes)
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Random Quote</h2>
-        </div>
-        <p className="App-intro">
-          <Quote />
-        </p>
-        <button>New Quote</button>
+        <Quote />
+        {this.state.quotes.quote} {this.state.quotes.author}
       </div>
     );
   }
