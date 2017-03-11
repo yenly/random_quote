5019b1bf6ed95d50d8e4d789f6bf5d895fce07b2
Yenly Ma <yencodes@gmail.com>
2017-03-11 14:42:23 -0800

    Add logic to use random number based on number of quotes to display random quote.

--- src/App.js
+++ src/App.js
@@ -8,35 +8,45 @@ class App extends Component {
     super();
     this.state = {
       quotes: {},
-      quoteID: 1,
+      quoteID: this.getRandomInt(16),
+      quote: {},
       quoteHistory: []
     };
   }
 
   componentDidMount() {
     const rootRef = firebase.database().ref().child('quotes');
-    const quotesRef = rootRef.child(this.state.quoteID);
-    quotesRef.on('value', snap => {
+    // const quotesRef = rootRef.child(this.state.quoteID);
+    rootRef.on('value', snap => {
       this.setState({
-        quotes: snap.val()
+        quotes: snap.val(),
       });
+      this.getQuote();
     });
   }
 
-  getNewQuote() {
+  getRandomInt(max) {
+    return Math.floor(Math.random() * (max));
+  }
 
+  getQuote() {
+    const newQuoteId = this.getRandomInt(this.state.quotes.length);
+    this.setState({
+      quoteID: newQuoteId,
+      quote: this.state.quotes[this.state.quoteID]
+    });
   }
 
   render() {
-    const tweetText = `http://twitter.com/home?status=\"${this.state.quotes.quote}\" by ${this.state.quotes.author}`;
+    const tweetText = `http://twitter.com/home?status=${this.state.quote.quote} by ${this.state.quote.author}`;
 
     return (
       <div className="app">
         <h1>Random Quote Machine</h1>
-        <p className="app-intro">Here's a random quote from my favorites list. I hope you find it as thought provoking as I do.</p>
+        <p className="app-intro">Here's a random quote from my favorites collection. I hope you find it as thought provoking as I do.</p>
         <Quote
-          words={this.state.quotes.quote}
-          author={this.state.quotes.author} />
+          words={this.state.quote.quote}
+          author={this.state.quote.author} />
         <div className="quote-panel-control">
           <a href={tweetText}><i className="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
           <button>New Quote</button>
