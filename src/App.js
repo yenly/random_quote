66be7d6fe0907a8ef6464d4cc9ab577cc0ca26c0
Yenly Ma <yencodes@gmail.com>
2017-03-11 15:45:40 -0800

    Add logic to new quote button to display new random quote.

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ class App extends Component {
       quote: {},
       quoteHistory: []
     };
+
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -30,42 +32,56 @@ class App extends Component {
   }
 
   getQuote() {
-    const newQuoteId = this.getRandomInt(this.state.quotes.length);
-    this.setState({
-      quoteID: newQuoteId,
-      quoteLen: this.state.quotes.length
-    });
-    this.setState({
-      quote: this.state.quotes[this.state.quoteID]
-    });
-    this.addToQuoteHistory(this.state.quoteID);
+    let newQuoteId = this.getRandomInt(this.state.quotes.length);
+    // if(!this.state.quoteHistory.includes(newQuoteId)) {
+    //   console.log(this.state.quoteHistory, newQuoteId, "add" )
+    // }
+    if (newQuoteId !== this.state.quoteID) {
+      this.setState({
+        quoteID: newQuoteId,
+        quoteLen: this.state.quotes.length
+      });
+      this.setState({
+        quote: this.state.quotes[this.state.quoteID]
+      });
+      // this.addToQuoteHistory(this.state.quoteID);
+    } else {
+      newQuoteId = this.getRandomInt(this.state.quotes.length);
+      console.log("Same quote ID as current!");
+    }
   }
 
-  addToQuoteHistory(num) {
-    let newArray = this.state.quoteHistory.slice();
-    newArray.push(num);
-    this.setState({
-      quoteHistory: newArray
-    });
+  // addToQuoteHistory(num) {
+  //   let newArray = this.state.quoteHistory.slice();
+  //   newArray.push(num);
+  //   this.setState({
+  //     quoteHistory: newArray
+  //   });
+  // }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+    this.getQuote();
   }
 
   render() {
-    console.log("after", this.state.quoteHistory, this.state.quoteLen);
+    // console.log("inside render", this.state.quoteHistory);
 
     const tweetText = `http://twitter.com/home?status=${this.state.quote.quote} by ${this.state.quote.author}`;
 
     return (
       <div className="app">
-        <h1>Random Quote Machine</h1>
-        <p className="app-intro">Here's a random quote from my favorites collection. I hope you find it as thought provoking as I do.</p>
-        <Quote
-          words={this.state.quote.quote}
-          author={this.state.quote.author} />
-        <div className="quote-panel-control">
-          <a href={tweetText}><i className="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
-          <button>New Quote</button>
-        </div>
-
+        <form onSubmit={this.onFormSubmit}>
+          <h1>Random Quote Machine</h1>
+          <p className="app-intro">Here's a random quote from my favorites collection. I hope you find it as thought provoking as I do.</p>
+          <Quote
+            words={this.state.quote.quote}
+            author={this.state.quote.author} />
+          <div className="quote-panel-control">
+            <a href={tweetText}><i className="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
+            <button type="submit">New Quote</button>
+          </div>
+        </form>
       </div>
     );
   }
